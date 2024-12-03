1cfdf773244c90399341a7e160f094104c437fd9
Rye1606Zz <49356827+thanhsonopera@users.noreply.github.com>
2024-12-03 22:26:57 +0700

    up gd

--- ultis.py
+++ ultis.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import numpy as np
+from PIL import Image
 
 
 def get_all_image_paths(directory):
@@ -49,3 +50,44 @@ def save_all_features(all_features, dataset):
 def load_dataset(path):
     data = np.load(path)
     return [data[f"arr_{i}"] for i in range(len(data.files))]
+
+
+def extract_hessian_sift_features(image_path):
+
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    sobel_xx = cv2.Sobel(image, cv2.CV_64F, 2, 0, ksize=3)
+    sobel_yy = cv2.Sobel(image, cv2.CV_64F, 0, 2, ksize=3)
+    sobel_xy = cv2.Sobel(image, cv2.CV_64F, 1, 1, ksize=3)
+
+    # Compute determinant and trace for the Hessian matrix
+    det_hessian = sobel_xx * sobel_yy - sobel_xy**2
+    trace_hessian = sobel_xx + sobel_yy
+    print('Det', det_hessian.shape, np.mean(det_hessian),
+          np.max(det_hessian), np.min(det_hessian))
+    # Filter potential keypoints based on determinant and trace
+    keypoints = np.where(det_hessian > 7000)  # threshold for robustness
+    # Visualize keypoints
+    keypoint_coords = [cv2.KeyPoint(
+        float(pt[1]), float(pt[0]), 1) for pt in zip(*keypoints)]
+    sift = cv2.SIFT_create()
+    _, descriptors = sift.compute(image, keypoint_coords)
+    return _, descriptors
+
+
+def rotate(image_path, angle=0):
+    if angle == 0 or angle == 360:
+        return cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    image = Image.open(image_path)
+    rotated_image = image.rotate(angle, expand=True)
+    cv_image = cv2.cvtColor(np.array(rotated_image), cv2.COLOR_BGR2RGB)
+    cv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2GRAY)
+    # cv2.imshow("Rotated Image", cv_image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return cv_image
+
+
+def extract(image):
+    sift = cv2.SIFT_create()
+    key_points, descriptors = sift.detectAndCompute(image, None)
+    return key_points, descriptors
